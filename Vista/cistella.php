de23ec8614e29215962da1ce083b59af4fc50eeb
Pau <Portatil@Portatil-PC>
2012-05-26 12:55:36 +0200

    Cesta Disseny OK

--- Vista/cistella.php
+++ Vista/cistella.php
@@ -24,10 +24,36 @@ require_once '../Logica/Atraccions.php';
                 unset($_SESSION["carro"][$id])
             }
         </script>
+          <style type="text/css">
+            table.hovertable {
+                    font-family: verdana,arial,sans-serif;
+                    font-size:11px;
+                    color:#333333;
+                    border-width: 1px;
+                    border-color: #999999;
+                    border-collapse: collapse;
+            }
+            table.hovertable th {
+                    background-color:#c3dde0;
+                    border-width: 1px;
+                    padding: 8px;
+                    border-style: solid;
+                    border-color: #a9c6c9;
+            }
+            table.hovertable tr {
+                    background-color:#d4e3e5;
+            }
+            table.hovertable td {
+                    border-width: 1px;
+                    padding: 8px;
+                    border-style: solid;
+                    border-color: #a9c6c9;
+            }
+        </style>
     </head>
     <body>
         <div id ="divSistella" >
-            <table id="taulaCistella">
+            <table id="taulaCistella" class="hovertable">
                 <tr>
                     <td><b>Numero</b></td>
                     <td><b>Nombre</b></td>
@@ -41,7 +67,7 @@ require_once '../Logica/Atraccions.php';
                     $atr = new Atraccions();
                     for ($i = 0; $i < $numCompres; $i++){
                         $resultAtr = $atr->getAtraccionByID($_SESSION["carro"][$i]["idAtraccio"]);
-                        echo "<tr>";
+                        echo "<tr onmouseover=\"this.style.backgroundColor='#ffff66';\" onmouseout=\"this.style.backgroundColor='#d4e3e5';\">";
                             echo "<td>". ($i+1) ."</td>";
                             echo "<td>".utf8_encode(mysql_result($resultAtr,0,1))."</td>";
                             echo "<td> <input type='text' size = 2 value = '".$_SESSION["carro"][$i]["quantitat"]."'> </td>";
