027fc4dc0bc63b789b789d3b867e5f824f0941e2
Pau <Portatil@Portatil-PC>
2012-06-13 10:03:13 +0200

    Login Admin

---
+++ Vista/administrator/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Administrador</title>
+        <link rel="stylesheet" href="../css/estil.css" type="text/css" media="screen" />
+    </head>
+    <body>
+        <div id="login">
+            Login
+            
+            
+        </div>
+    </body>
+</html>
